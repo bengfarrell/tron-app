8d6dbacf58ce698abc645b791354317f825fdb47
farrell <farrell@adobe.com>
2015-12-01 17:43:07 -0800

    dealt with on ready timing

--- src/tron-app.js
+++ src/tron-app.js
@@ -6,11 +6,11 @@ var tronapp = function(config) {
     /** reference to main window */
     this.mainWindow = null;
 
-    /** reference to native Atom-Shell app instance */
-    this.application = null;
-
     /** configuration */
     this.config = new tron.config(config);
+
+    /** reference to native Atom-Shell app instance */
+    this.application = require('app');
 
     /**
      * start the app
@@ -19,19 +19,29 @@ var tronapp = function(config) {
     this.start = function() {
         var tron = require('../lib/tron.js');
 
-        this.application = require('app');
         require('crash-reporter').start();
 
         this.application.on('window-all-closed', function() {
             self.application.quit();
         });
 
-        this.application.on('ready', function() {
-            self.mainWindow = new tron.windows().create(self.config);
-
-            self.mainWindow.on('closed', function () {
-                self.mainWindow = null;
+        if (this.application.isReady()) {
+            self.spawnWindow();
+        } else {
+            self.application.on('ready', function() {
+                self.spawnWindow();
             });
+        }
+    };
+
+    /**
+     * spawn the main application window
+     */
+    this.spawnWindow = function() {
+        self.mainWindow = new tron.windows().create(self.config);
+
+        self.mainWindow.on('closed', function () {
+            self.mainWindow = null;
         });
     };
 
